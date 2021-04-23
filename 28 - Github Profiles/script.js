7aaa26f2af6ed14bde37000c4184bb15dd00a4cc
Gian Michel <gianmnf@unipam.edu.br>
2021-04-22 21:30:43 -0300

    Adding Repos To Card

--- 28 - Github Profiles/script.js
+++ 28 - Github Profiles/script.js
@@ -8,10 +8,20 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
         createUserCard(data)
+        getRepos(username)
     } catch(err) {
         if(err.response.status == 404) {
             createErrorCard('No profile with this username')
         }        
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
     }
 }
 
@@ -46,6 +56,22 @@ function createErrorCard(msg) {
      `
 
      main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+    
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
 }
 
 form.addEventListener('submit', (e) => {
